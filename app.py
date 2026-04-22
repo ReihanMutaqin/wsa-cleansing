4abf7323035771757465d13ff65a69988e89442a
ReihanMutaqin <60320409+ReihanMutaqin@users.noreply.github.com>
2026-04-22 15:59:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,48 @@ from datetime import datetime
 # 1. KONFIGURASI HALAMAN & CSS
 # ==========================================
 st.set_page_config(page_title="TOOLS FILTER TELKOM", layout="wide")
+# ==========================================
+# MAINTENANCE MODE
+# ==========================================
+st.markdown("""
+    <style>
+    .center-box {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 80vh;
+        text-align: center;
+    }
+    .btn-link {
+        background: linear-gradient(45deg, #00d4ff, #008fb3);
+        padding: 12px 25px;
+        border-radius: 10px;
+        color: black;
+        font-weight: bold;
+        text-decoration: none;
+        margin-top: 20px;
+        display: inline-block;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+st.markdown("""
+<div class="center-box">
+    <h1>🚧 Website Dipindahkan</h1>
+    <p>Tools ini sudah tidak digunakan lagi.</p>
+    <p>Silakan gunakan versi terbaru di link berikut:</p>
+    <a href="https://filter-sakti.vercel.app/" target="_blank" class="btn-link">
+        🔗 Buka Website Baru
+    </a>
+</div>
+""", unsafe_allow_html=True)
+
+st.stop()
+
+
+
+
 
 st.markdown("""
     <style>
